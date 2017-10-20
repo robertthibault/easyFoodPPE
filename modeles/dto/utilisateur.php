64d49ef4ccc8fa87316842372777ec93196f15ea
Roustin420 <31992584+Roustin420@users.noreply.github.com>
2017-10-20 13:53:38 +0200

    ajout de la fonction hydrate dans la classe utilisateur

--- modeles/dto/utilisateur.php
+++ modeles/dto/utilisateur.php
@@ -2,7 +2,7 @@
 class Utilisateur{
     private $login;
     private $mdp;
-    
+
     public function __construct($unLogin  , $unMdp ){
         $this->login = $unLogin;
         $this->mdp = $unMdp;
@@ -19,5 +19,15 @@ class Utilisateur{
     public function setMdp($unMdp){
         $this->mdp = $unMdp;
     }
+    public function hydrate(array $donnees)
+	{
+		foreach ($donnees as $key => $value)
+		{
+			$method = 'set'.ucfirst($key);
+			if (method_exists($this, $method))
+			{
+				$this->$method($value);
+			}
+		}
+	}
 }
-
