dfa673434a992a43112c0238160ab90614437c14
Roustin420 <31992584+Roustin420@users.noreply.github.com>
2017-10-20 16:26:49 +0200

    ajout de la classe plat + suppression d'images

---
+++ modeles/dto/plat.php
@@ -0,0 +1,73 @@
+<?php
+
+class Plat
+{
+  private $idP;
+  private $idR;
+  private $idT;
+  private $nomP;
+  private $prixFrounisseurP;
+  private $prixClientP;
+  private $platVisible;
+  private $descriptionP;
+
+
+
+  function __construct($unIdR,$unIdT,$unNomP,$unPrixFournisseurP,$unPrixClientP,$estVisible,$uneDescriptionP)
+  {
+    $this->idP=platDAO::dernierNumero()+1;
+    $this->idR=$unIdR;
+    $this->idT=$unIdT;
+    $this->nomP=$unNomP;
+    $this->prixFournisseurP=$unPrixFournisseurP;
+    $this->prixClientP=$unPrixClientP;
+    $this->platVisible=$estVisible;
+    $this->descriptionP=$uneDescriptionP;
+
+  }
+  public function getidP(){
+    return $this->idP;
+  }
+  public function getIdT(){
+    return $this->idT;
+  }
+  public function setIDT($unIdT){
+    $this->idT=$unIdT;
+  }
+  public function getnomP(){
+    return $this->nomP;
+  }
+  public function setNomP($unNomP){
+    $this->nomP=$unNomP;
+  }
+  public function getPrixFournisseurP(){
+    return $this->prixFournisseurP;
+  }
+  public function setPrixFournisseurP($unPrixFournisseurP){
+    $this->prixFournisseurP = $unPrixFournisseurP;
+  }
+  public function getPrixClientP(){
+    return $this->prixClientP;
+  }
+  public function setPrixClientP($unPrixClientP){
+    $this->prixClientP = $unPrixClientP;
+  }
+  public function getPlatVisible(){
+    return $this->platVisible;
+  }
+  public function setPlatVisible($estVisible){
+    $this->platVisible = $estVisible;
+  }
+  public function getDescriptionP(){
+    return $this->descriptionP
+  }
+  public function setDescriptionP($uneDescriptionP){
+    $this->descriptionP = $uneDescriptionP;
+  }
+
+}
+
+
+
+
+ ?>
