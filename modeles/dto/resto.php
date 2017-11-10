b953aad548f9b99acd0ffd45fa63c4b78051e7b1
Alexandre <31766499+AlexandreVias@users.noreply.github.com>
2017-11-10 15:13:12 +0100

    Ajout des fonctions hydrate

--- modeles/dto/resto.php
+++ modeles/dto/resto.php
@@ -59,6 +59,15 @@ class Resto{
     $this->villeR = $uneVilleR;
   }
 
+  public function hydrate(array $donnees){
+		foreach ($donnees as $key => $value){
+			$method = 'set'.ucfirst($key);
+			if (method_exists($this, $method)){
+				$this->$method($value);
+			}
+		}
+	}
+
 }
 
 
--- modeles/dto/typePlat.php
+++ modeles/dto/typePlat.php
@@ -19,6 +19,15 @@ class TypePlat{
     $this->libelleT = $unLibelleT;
   }
 
+  public function hydrate(array $donnees){
+		foreach ($donnees as $key => $value){
+			$method = 'set'.ucfirst($key);
+			if (method_exists($this, $method)){
+				$this->$method($value);
+			}
+		}
+	}
+
 }
 
  ?>
