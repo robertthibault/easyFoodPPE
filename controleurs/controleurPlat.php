90a873602ca68c487e65f5f7e8cae42dc1b048e7
Roustin420 <31992584+Roustin420@users.noreply.github.com>
2017-11-10 16:39:03 +0100

    aaa

--- controleurs/controleurPlat.php
+++ controleurs/controleurPlat.php
@@ -3,15 +3,15 @@
 $plats = platDAO::lesPlats();
 
 $formulairePlat = new Formulaire('post', 'index.php', 'fPlat', '');
+
 foreach ($plats as $plat) {
+
   $formulairePlat->ajouterComposantLigne($formulairePlat->creerInputImage("images/".$plat->nomP.".jpg"),2);
   $formulairePlat->ajouterComposantLigne($formulairePlat->creerLabel($plat->nomP), 1);
   $formulairePlat->ajouterComposantLigne($formulairePlat->creerInputButton("Ajouter","Ajouter_".$plat->nomP), 1);
   $formulairePlat->ajouterComposantTab();
 
 }
-
-
 
 $formulairePlat->creerFormulaire();
 
