97297366ae628b8edcba4bc5f6e01eece589a6cf
Roustin420 <31992584+Roustin420@users.noreply.github.com>
2017-12-01 15:35:42 +0100

    Add files via upload

---
+++ squelettePlat.php
@@ -0,0 +1,17 @@
+<div classe="conteneur">
+	<header>
+		<?php include 'haut.php' ;?>
+	</header>
+
+	<div id="Plat">
+			<p id ='titre'></p>
+				<main>
+					<?php echo $tablePlat; ?>
+			</main>
+	</div>
+
+	<footer>
+		<?php include 'bas.php' ;?>
+	</footer>
+
+</div>
